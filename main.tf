810dc80a35232e02b1492e93ff10f937051fd3c9
tyagmeg <122991725+tyagmeg@users.noreply.github.com>
2023-01-20 10:26:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,12 +17,21 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default"{
 default= true
 }
-
+module "blog_sg" {
+  source              = "terraform-aws-modules/security-group/aws"
+  version             = "4.17.1"
+  vpc_id              = data.aws_vpc.default.id
+  ingress_rule        = ["http-80-tcp,""https-443-tcp"]
+  ingress_cider_blocks= ["0.0.0.0/0"]
+  egress_rule        = ["all-all"]
+  egress_cider_blocks= ["0.0.0.0/0"]
+}
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids =[aws_security_group.blog.id]
+  name="blog_new"
+  vpc_security_group_ids =[module.blog_sg.security_group_id]
   tags = {
     Name = "HelloWorld"
   }
