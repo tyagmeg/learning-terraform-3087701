f206340fc2e16ecdb116d67f21fa0ca086348bde
tyagmeg <122991725+tyagmeg@users.noreply.github.com>
2023-01-20 10:51:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,44 +38,4 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_security_group" "blog" {
- name       = "Blog"
- description= "Allow http and https in allow everything"
- vpc_id= data.aws_vpc.default.id
-}
 
-resource "aws_security_group_rule" "blog_http_in" {
-  
-  type      = "ingress"
-  from_port = 80
-  to_port   = 80
-  protocol  = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = aws_security_group.blog.id
-  
-}
-
-resource "aws_security_group_rule" "blog_evrything_out" {
-  
-  type      = "egress"
-  from_port = 0
-  to_port   = 0
-  protocol  = "-1"
-  cidr_blocks= ["0.0.0.0/0"]
-  
-  security_group_id = aws_security_group.blog.id
-  
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  
-  type      = "ingress"
-  from_port = 80
-  to_port   = 80
-  protocol  = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  
-  security_group_id = aws_security_group.blog.id
-  
-}
